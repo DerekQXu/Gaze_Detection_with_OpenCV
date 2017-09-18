4a3c76575b394cdd0e94df81b9b1ed4e53ce8391
Derek Xu <derekqxu@gmail.com>
2017-09-17 22:49:49 -0700

    Added some image processesing before Cascading
    More reliable pupil detection

--- script.py
+++ script.py
@@ -1,16 +1,36 @@
 import cv2
 import numpy as np
 
+#Define Queue
+class Queue:
+    def __init__(self):
+        self.items = []
+    def isEmpty(self):
+        return self.items == []
+    def enqueue(self,item):
+        self.items.insert(0,item)
+    def dequeue(self):
+        self.items.pop()
+    def size(self):
+        return len(self.items)
+
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 cap = cv2.VideoCapture(0)
+pupilArrayLeftX = Queue()
+pupilArrayLeftY = Queue()
+pupilArrayRightX = Queue()
+pupilArrayRightY = Queue()
 
 while True:
     #setup
     ret, img = cap.read()
     img = cv2.medianBlur(img,5)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    #fix illuminescence + increase contrast
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    gray = clahe.apply(gray)
     #detect face
     faces = face_cascade.detectMultiScale(gray, 1.3, 10)
     for (x,y,w,h) in faces:
@@ -29,10 +49,22 @@ while True:
             proi_gray = eroi_gray[ey:ey+eh, ex:ex+ew]
             proi_color = eroi_color[ey:ey+eh, ex:ex+ew]
             #threshold
-            ret, proi_mask = cv2.threshold(proi_gray, 50, 255,
+            minbrightness = 255
+            for i in range(0, ew-1):
+                for j in range(0, int(eh*3/4)):
+                    if proi_gray[i,j] < minbrightness:
+                        minbrightness = proi_gray[i,j]
+            ret, proi_mask = cv2.threshold(proi_gray, (minbrightness+2), 255,
                     cv2.THRESH_BINARY)
             proi_gray = cv2.bitwise_and(proi_gray, proi_gray, mask=proi_mask)
             proi_gray[proi_mask > 0] = 255
+            #find corner of eyes
+            '''
+            proi_gray_fix = np.float32(proi_gray)
+            dst = cv2.cornerHarris(proi_gray_fix,2,3,0.04)
+            dst = cv2.dilate(dst,None)
+            proi_color[dst>0.01*dst.max()] = [0,255,0]
+            '''
             #average blob coordinates
             count = 0
             averageX = 0
@@ -55,6 +87,7 @@ while True:
                     proi_color[px, py] = [0, 0, 255]
     #draw face
     cv2.imshow('img', img)
+    #cv2.imshow('debug1',gray)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
