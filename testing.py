14642280e44f1bfd99245ff35a5bdc6d88f777d0
Derek Xu <derekqxu@gmail.com>
2017-11-20 20:40:18 -0800

    Add files via upload
    testing.py works now! :D
    
    TODO:
    add comments

--- testing.py
+++ testing.py
@@ -16,14 +16,17 @@ class PupilDetection:
     def __init__(self):
         self.left_pupil_q = deque()
         self.right_pupil_q = deque()
-        temp = Coord()
-        self.left_pupil_q.append(temp)
-        self.right_pupil_q.append(temp)
+        self.pupil_temp1 = Coord()
+        self.pupil_temp2 = Coord()
+        self.left_pupil_q.append(self.pupil_temp1)
+        self.right_pupil_q.append(self.pupil_temp2)
         #note in order: left_x, right_x, left_y, right_y
         self.q_size = 0
         self.face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
         self.eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
         self.cap = cv2.VideoCapture(0)
+        ret, temp = self.cap.read()
+        self.height, self.width, self.channels = temp.shape
         self.face = []
         self.eyes = []
     def __del__(self):
@@ -37,24 +40,34 @@ class PupilDetection:
         self.faces = self.face_cascade.detectMultiScale(self.gray, 1.3, 8)
     def getEyes(self):
         self.getFace()
+        #print("detects %d faces" % (len(self.face)))
+        #if len(self.face) != 1:
+        #    return
         for (x,y,w,h) in self.faces:
+            self.pupil_temp1.x = x
+            self.pupil_temp1.y = int(y+h/5)
+            self.pupil_temp2.x = x
+            self.pupil_temp2.y = int(y+h/5)
             self.eroi_gray = cv2.medianBlur(self.gray[int(y+h/5):int(y+h/1.8), x:x+w], 5)
-            self.eroi_color = self.color[int(y+h/5):int(y+h/1.8), x:x+w]
             self.eyes = self.eye_cascade.detectMultiScale(self.eroi_gray, 1.3, 5)
     def getPupils(self):
         self.getEyes()
-        if len(self.eyes) != 2:
-            print("cannot detect 2 eyes")
-            return
+        #print("detects %d eyes" % (len(self.eyes)))
+        #if len(self.eyes) != 2:
+        #    return
         eye1 = Coord()
         eye2 = Coord()
         self.q_size += 1
         count = 0
         for (x,y,w,h) in self.eyes:
+            if count == 0:
+                self.pupil_temp1.x += x
+                self.pupil_temp1.y += y
+            else:
+                self.pupil_temp2.x += x
+                self.pupil_temp2.y += y
             #setup
             proi_gray = self.eroi_gray[y:y+h, x:x+w]
-            #TODO: edit this
-            self.proi_color = self.eroi_color[y:y+h, x:x+w]
             #threshold
             minbrightness = 255
             for i in range(0, w):
@@ -70,16 +83,21 @@ class PupilDetection:
             for px in range(0, w):
                 for py in range(0, h):
                     if proi_gray[px,py] < 50:
-                        listX.append(px)
-                        listY.append(py)
+                        listX.append(py)
+                        listY.append(px)
             #TODO: change this
             if count == 0:
-                eye1.x = int(statistics.median(listX))
-                eye1.y = int(statistics.median(listY))
+                eye1.x = self.pupil_temp1.x + int(statistics.median(listX))
+                eye1.y = self.pupil_temp1.y + int(statistics.median(listY))
             else:
-                eye2.x = int(statistics.median(listX))
-                eye2.y = int(statistics.median(listY))
+                eye2.x = self.pupil_temp2.x + int(statistics.median(listX))
+                eye2.y = self.pupil_temp2.y + int(statistics.median(listY))
+            count += 1
         #TODO: change implementation of this
+        print("===================")
+        print("eye1 locale: (%d, %d)" % (eye1.x, eye1.y))
+        print("eye2 loclae: (%d, %d)" % (eye2.x, eye2.y))
+        print("===================")
         if eye1.getX() < eye2.getY():
             self.left_pupil_q.append(eye1)
             self.right_pupil_q.append(eye2)
@@ -101,11 +119,21 @@ class PupilDetection:
                 self.proi_color[px, py] = [0,0,255]
         '''
         #for coord in self.right_pupil_q:
-        coord = self.right_pupil_q[0]
-        for px in range(coord.x-3, coord.x+3):
-            for py in range(coord.y-3, coord.y+3):
-                self.proi_color[px, py] = [0,0,255]
+        for i in range(0, len(self.right_pupil_q)):
+            self.drawPupils((self.right_pupil_q[i]).x, (self.right_pupil_q[i]).y)
+        for i in range(0, len(self.left_pupil_q)):
+            self.drawPupils((self.left_pupil_q[i]).x, (self.left_pupil_q[i]).y)
         cv2.imshow('img', self.color)
+    def drawPupils(self, x, y):
+        if x > self.height or x < 0:
+            print ("attempting to print out of range")
+            return
+        if y > self.width or y < 0:
+            print ("attempting to print out of range")
+            return
+        for px in range(x-3, x+3):
+            for py in range(y-3, y+3):
+                self.color[py, px] = [0,0,255]
 
 
 def main():
